063a4ada0bf1a737c2f5a6d7bf6f42e7935acf10
jacudibu <jacudibu@gmail.com>
2023-05-30 20:36:54 +0200

    Added a placeholder /move command

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,25 @@ struct Data {} // User data, which is stored and accessible in all command invoc
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
 
-/// Displays your or another user's account creation date
-#[poise::command(slash_command, prefix_command)]
+#[poise::command(slash_command)]
 async fn ping(ctx: Context<'_>) -> Result<(), Error> {
     ctx.say("pong").await?;
+    Ok(())
+}
+
+/// Receive Magicarps' blessings!
+#[poise::command(slash_command, rename = "move")]
+async fn skill(
+    ctx: Context<'_>,
+    #[description = "Which move?"] #[rename = "move"] skill: String,
+) -> Result<(), Error> {
+    ctx.say("__Splash__
+*The user just flops splashing some water, this has no effect at all...*
+**Type**: Normal -- **Support**
+**Target**: User -- **Power**: 0
+**Damage Dice**: None
+**Accuracy Dice**: DEXTERITY + BRAWL
+**Effect**: -").await?;
     Ok(())
 }
 
@@ -15,7 +30,7 @@ async fn ping(ctx: Context<'_>) -> Result<(), Error> {
 async fn main() {
     let framework = poise::Framework::builder()
         .options(poise::FrameworkOptions {
-            commands: vec![ping()],
+            commands: vec![ping(), skill()],
             ..Default::default()
         })
         .token(std::env::var("DISCORD_TOKEN").expect("missing DISCORD_TOKEN"))
