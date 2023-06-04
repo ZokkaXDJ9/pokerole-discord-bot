b58b485df713c63f2ea848139162ca24c3d08c3c
jacudibu <jacudibu@gmail.com>
2023-06-04 19:45:03 +0200

    Added descriptions to /timestamps values

--- src/commands/timestamp.rs
+++ src/commands/timestamp.rs
@@ -1,36 +1,35 @@
 use crate::commands::{Context, Error};
 use chrono::{Datelike, NaiveDate, NaiveDateTime, Timelike, TimeZone, Utc};
 
-/// Create a timestamp.
+/// Create a timestamp based of a UTC date.
 #[poise::command(slash_command)]
 pub async fn timestamp(
     ctx: Context<'_>,
+    #[description = "Which minute? Default now."]
     minute: Option<u8>,
+    #[description = "Which hour? Default now."]
     hour: Option<u8>,
+    #[description = "Which day? Default today."]
     day: Option<u8>,
+    #[description = "Which month? Default this month."]
     month: Option<u8>,
+    #[description = "Which year? Default this year."]
     year: Option<u16>,
 ) -> Result<(), Error> {
-
-    // Get the current date and time in UTC
     let current_datetime = Utc::now().naive_utc();
 
-    // Get the components from the input options or use the current components as defaults
     let minute = minute.unwrap_or(current_datetime.minute() as u8);
     let hour = hour.unwrap_or(current_datetime.hour() as u8);
     let day = day.unwrap_or(current_datetime.day() as u8);
     let month = month.unwrap_or(current_datetime.month() as u8);
     let year = year.unwrap_or(current_datetime.year() as u16);
 
-    // Create a NaiveDateTime object from the components in UTC
-    let timestamp = NaiveDate::from_ymd_opt(year.into(), month.into(), day.into()).expect("Should be valid!")
-        .and_hms_opt(hour.into(), minute.into(), 0).unwrap();
+    let timestamp = NaiveDate::from_ymd_opt(year.into(), month.into(), day.into()).expect("ymd should be valid!")
+        .and_hms_opt(hour.into(), minute.into(), 0).expect("hms should be valid!");
     let timestamp_utc = Utc.from_local_datetime(&timestamp).unwrap().naive_utc();
 
-    // Convert the NaiveDateTime to Unix timestamp (seconds since January 1, 1970)
     let unix_timestamp = timestamp_utc.timestamp();
 
-    // Send the Unix timestamp as a response in the Discord channel
     ctx.say(std::format!("<t:{0}:f> | <t:{0}:R>", unix_timestamp.to_string())).await?;
 
     Ok(())
