1519c307d1c06000448e8fc2fd317fbf14d44d32
jacudibu <jacudibu@gmail.com>
2023-06-04 06:03:11 +0200

    Added more /rules

--- src/main.rs
+++ src/main.rs
@@ -465,6 +465,7 @@ async fn main() {
             name: String::from("Levels and Ranks"),
             flavor: String::from("Now, would you look at this shiny badge?"),
             text: String::from("A level up always requires 100 Experience Points. Experience is gained by going on quests and joining in on events, depending on duration and difficulty/danger levels! For each level up, you gain 1 Stat point, which you may freely spend to increase your stats (see Limit Breaking in case you are maxed out.)\n\
+            You'll gain one combat stat point to allocate for each level up!\n\
             \n\
             Furthermore, Levels decide your guild rank and when you can evolve:\n\
             Level 2 => Silver\n\
@@ -472,12 +473,56 @@ async fn main() {
             Level 4 => Gold\n\
             Level 6 => Evo #2\n\
             Level 8 => Platinum\n\
-            Level 16 => Diamond\n"),
+            Level 16 => Diamond\n\
+            \n\
+            Besides the extra die for all the *+ Rank* accuracy rolls, you'll gain 2 social stat points for each rank up!"),
             example: String::from("")
         }, GameRule {
             name: String::from("Wound damage"),
             flavor: String::from("It's just a flesh wound!"),
             text: String::from("Wound damage reduces your maximum HP and needs to be healed over time in base and/or by visiting a doctor. If your max HP falls to 0, you lose consciousness and really need some medical assistance!"),
+            example: String::from("")
+        }, GameRule {
+            name: String::from("Stat Changes"),
+            flavor: String::from(""),
+            text: String::from("Stat changes done by different moves stack up to a maximum of +/- 3.\n\
+            Critical stat changes apply/reduce one additional stat point, but not past the limit! If it would boost multiple stats, you got to choose one which will receive the crit bonus.\n\
+            Stats cannot go below 0."),
+            example: String::from("")
+        }, GameRule {
+            name: String::from("Alternative Moves (Dodge, Struggle, Help Another)"),
+            flavor: String::from(""),
+            text: String::from("Every Character has access to the following moves at any time:\n\
+            - Struggle\n\
+            - Help Another\n\
+            - Dodge\n"),
+            example: String::from("")
+        }, GameRule {
+            name: String::from("Accuracy Reduction"),
+            flavor: String::from(""),
+            text: String::from("Characters afflicted by accuracy reduction (either as a stat change or through a move effect) still roll the regular amount of accuracy dies, but then subtract -X from the amount of successes this roll had."),
+            example: String::from("Let's say you want to use Rock Slide. The move has a -1 Accuracy effect. You roll your regular Dex+Rank amount of dies for your accuracy and land two successes. Now we reduce those successes by -1... which leaves us with one success, so the attack still hits, yay!")
+        }, GameRule {
+            name: String::from("Defense"),
+            flavor: String::from(""),
+            text: String::from("You Physical Defense is `Vitality / 2`.\nYour Special Defense is `Insight / 2`.\n\nMinimum damage is always 1, unless you resist the attack's type - this is the only case where it gets reduced to 0."),
+            example: String::from("With 4 points in Vitality and 5 points in Insight, you'd get 2 physical and 3 special defense!")
+        }, GameRule {
+            name: String::from("Orre Character Import"),
+            flavor: String::from("Celebi did another woopsiedoodle!"),
+            text: String::from("While everything that happened over in the Orre Region is considered non-canon since this is taking place in a different timeline and in the far future, you can import your characters from Orre over to this system!\n\
+            If you where at least Silver Rank, your character may start with Level 2 here.\n\
+            If you where Gold or higher, your character may start with Level 3.\n\
+            All of these are optional, of course! And no items or other unlocks can be carried over.\n\
+            It is fine to start out as an evolved mon here, just check `/rule Evolution` for the details.\n\
+            Just follow the character creation guide and apply your two level ups afterwards, those will yield you one Combat Stat Point each and promote you to silver, which also yields two extra Social Stat points!"),
+            example: String::from("")
+        }, GameRule {
+            name: String::from("Character Slots"),
+            flavor: String::from(""),
+            text: String::from("You get one new character slot ever 5 levels (cumulative across all your characters!).\n\
+            Using those or creating multiple characters is completely optional, of course.\n\
+            Also, if you want more or need one earlier, you may request that from our admin team! We'll vote internally on a case-by-case basis to determine if it would work out."),
             example: String::from("")
         }
     ];
