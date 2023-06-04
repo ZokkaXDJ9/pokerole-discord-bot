241c49b071d012488222394eb17e808789119cd3
jacudibu <jacudibu@gmail.com>
2023-06-04 07:35:35 +0200

    updated about

--- src/commands/about.rs
+++ src/commands/about.rs
@@ -4,6 +4,15 @@ use crate::commands::{Context, Error};
 pub async fn about(
     ctx: Context<'_>,
 ) -> Result<(), Error> {
-    ctx.say("Raw pokemon data courtesy by pokeapi (https://github.com/PokeAPI/pokeapi).").await?;
+    ctx.say("\
+Made with love and 🦀!
+
+Original Pokerole System: https://www.pokeroleproject.com/
+Original Pokerole CSV data taken from Pokerole-Discord.py-Base (https://github.com/XShadeSlayerXx/PokeRole-Discord.py-Base)
+With lots of custom edits to make the whole pokerole system fit our custom setting better.
+
+Raw pokemon data taken from pokeAPI (https://github.com/PokeAPI/pokeapi).\
+"
+    ).await?;
     Ok(())
 }
