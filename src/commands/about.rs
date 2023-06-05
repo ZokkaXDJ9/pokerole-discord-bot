9d2f266ac914301a9b421de8fb6f84bf4c695a71
jacudibu <jacudibu@gmail.com>
2023-06-05 19:57:48 +0200

    Updated about

--- src/commands/about.rs
+++ src/commands/about.rs
@@ -7,12 +7,11 @@ pub async fn about(
 ) -> Result<(), Error> {
     ctx.say("\
 Made with love and 🦀!
-
 ## Data Sources:
 - Original Pokerole System: https://www.pokeroleproject.com/
 - Weather & Status Effects taken from Pokerole-Discord.py-Base (https://github.com/XShadeSlayerXx/PokeRole-Discord.py-Base)
 - All other Pokerole specific data is taken from Pokerole-Data (https://github.com/Pokerole-Software-Development/Pokerole-Data)
-- Raw pokemon data taken from pokeAPI (https://github.com/PokeAPI/pokeapi).
+- Raw pokemon data taken from pokeAPI (https://github.com/PokeAPI/pokeapi)
 Thanks to all of these projects for the effort they put in! <3
 "
     ).await?;
