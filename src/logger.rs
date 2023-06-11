05972b989dc619dd2e513516c4dca9271411f267
jacudibu <jacudibu@gmail.com>
2023-06-11 08:47:51 +0200

    more logging

--- src/logger.rs
+++ src/logger.rs
@@ -4,5 +4,6 @@ use log::{LevelFilter};
 pub fn init_logging() {
     Builder::new()
         .format_module_path(true)
+        .filter(None, LevelFilter::Warn)
         .filter_module("pokerole_discord_bot", LevelFilter::max()).init();
 }
