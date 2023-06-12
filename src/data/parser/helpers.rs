4bd9357a3593c60b19b85a5eab70c746847ce11e
jacudibu <jacudibu@gmail.com>
2023-06-12 19:46:36 +0200

    Added an ignore list for Pokerole-Data items we don't wanna parse

--- src/data/parser/helpers.rs
+++ src/data/parser/helpers.rs
@@ -1,8 +1,11 @@
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
-use log::error;
+use log::{error, info};
 use serde::de::DeserializeOwned;
+
+/// Data Objects which should be ignored
+const REJECTED_DATA_FILE_NAMES: [&str; 4] = ["Any Move.json", "Potion.json", "Super Potion.json", "Hyper Potion.json"];
 
 fn parse_file<T: DeserializeOwned>(file_path: &str) -> Result<T, Box<dyn std::error::Error>> {
     let mut file = File::open(file_path)?;
@@ -17,15 +20,18 @@ pub fn parse_directory<P: AsRef<Path>, T: DeserializeOwned>(path: P) -> Vec<T> {
     let mut result = Vec::new();
 
     let entries = std::fs::read_dir(path).expect("Failed to read directory");
-    for entry in entries {
-        if let Ok(entry) = entry {
-            let file_path = entry.path();
+    for entry in entries.flatten() {
+        if REJECTED_DATA_FILE_NAMES.iter().any(|x| entry.path().ends_with(x)) {
+            info!("Skipping {:?}", entry.path());
+            continue;
+        }
 
-            if file_path.is_file() && file_path.extension().map_or(false, |ext| ext == "json") {
-                match parse_file::<T>(file_path.to_str().expect("")) {
-                    Ok(parsed) => result.push(parsed),
-                    Err(err) => error!("Failed to parse file {:?}: {}", file_path, err)
-                }
+        let file_path = entry.path();
+
+        if file_path.is_file() && file_path.extension().map_or(false, |ext| ext == "json") {
+            match parse_file::<T>(file_path.to_str().expect("")) {
+                Ok(parsed) => result.push(parsed),
+                Err(err) => error!("Failed to parse file {:?}: {}", file_path, err)
             }
         }
     }
