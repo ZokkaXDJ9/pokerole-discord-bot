685f413894c775cc07226e4d910cbf88941dd134
jacudibu <jacudibu@gmail.com>
2023-06-18 18:54:44 +0200

    Added a way to update character messages

---
+++ migrations/20230618163414_initial.sql
@@ -0,0 +1,8 @@
+CREATE TABLE characters(
+    user_id INTEGER NOT NULL,
+    name TEXT NOT NULL,
+    stat_message_id INTEGER NOT NULL,
+    stat_channel_id INTEGER NOT NULL,
+    experience INTEGER NOT NULL,
+    money INTEGER NOT NULL
+)
--- src/commands/initialize_character.rs
+++ src/commands/initialize_character.rs
@@ -14,17 +14,23 @@ pub async fn initialize_character(
     #[min = 0_i64]
     money: i64,
 ) -> Result<(), Error> {
-    let mut message = ctx.channel_id().send_message(ctx, |f|
-        f.content("[Placeholder. This should get replaced within a couple seconds.]")
+    let message = ctx.channel_id().send_message(ctx, |f|
+        f.content("[Placeholder. This should get replaced or deleted within a couple seconds.]")
     ).await?;
 
     let user_id = player.id.0 as i64;
     let stat_message_id = message.id.0 as i64;
+    let stat_channel_id = message.channel_id.0 as i64;
+
+    // TODO: Unique constraints
+    // TODO: also track guild_id to allow multi-server stuff
+
     let result = sqlx::query!(
-        "INSERT INTO characters (user_id, name, stat_message_id, experience, money) VALUES (?, ?, ?, ?, ?)",
+        "INSERT INTO characters (user_id, name, stat_message_id, stat_channel_id, experience, money) VALUES (?, ?, ?, ?, ?, ?)",
         user_id,
         name,
         stat_message_id,
+        stat_channel_id,
         exp,
         money
     ).execute(&ctx.data().database)
@@ -38,7 +44,7 @@ pub async fn initialize_character(
             ).await?;
         }
 
-        message.edit(ctx, |f| f.content(name)).await?;
+        update_character_post(&ctx, user_id, name).await?;
         return Ok(());
     }
 
@@ -48,6 +54,29 @@ pub async fn initialize_character(
     }).await?;
     message.delete(ctx).await?;
 
+    Ok(())
+}
+
+pub async fn update_character_post<'a>(ctx: &Context<'a>, user_id: i64, name: String) -> Result<(), Error> {
+    // "SELECT" will return to "entry" the rowid of the todo rows where the user_Id column = user_id.
+    let entry = sqlx::query!(
+                "SELECT experience, money, stat_message_id, stat_channel_id \
+                FROM characters WHERE user_id = ? AND name = ? \
+                ORDER BY rowid \
+                LIMIT 1",
+                user_id,
+                name,
+            )
+        .fetch_one(&ctx.data().database)
+        .await
+        .unwrap();
+
+    // TODO: Handle error in case message wasn't found
+
+    let message = ctx.serenity_context().http.get_message(entry.stat_channel_id as u64, entry.stat_message_id as u64).await;
+    if let Ok(mut message) = message {
+        message.edit(ctx, |f| f.content(format!("**{}**\n**Experience**: {}\n**Money**: {}", name, entry.experience, entry.money))).await?;
+    }
 
     Ok(())
 }
