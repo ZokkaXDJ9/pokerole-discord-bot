d6b968414d99ff12ab3089dcddb7af8c3d49da84
jacudibu <jacudibu@gmail.com>
2023-06-19 08:29:21 +0200

    Improved error handling

--- src/commands/characters/reward_money.rs
+++ src/commands/characters/reward_money.rs
@@ -34,7 +34,10 @@ pub async fn reward_money(
             ).execute(&ctx.data().database).await?;
 
             if result.rows_affected() != 1 {
-                return send_error(&ctx, "Something went wrong. Try again?").await;
+                return send_error(&ctx, "Something went wrong!\n\
+                You hit an absolute edge case where the value has been updated by someone else while this command has been running. \
+                If this seriously ever happens and/or turns into a problem, let me know. For now... try again? :'D\n\
+                You can copy the command string either by just pressing the up key inside the text field on pc.").await;
             }
 
             ctx.say(format!("{} received {} Poke!", name, amount)).await?;
