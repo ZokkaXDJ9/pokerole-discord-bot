d2002e04868ada43fcb24ab8c13c0fd3059da59c
jacudibu <jacudibu@gmail.com>
2023-06-20 07:27:49 +0200

    Character Cache now stores more than just the name, using that for guild and user aware autocompletion

--- src/cache.rs
+++ src/cache.rs
@@ -2,29 +2,58 @@ use tokio::sync::Mutex;
 use sqlx::{Pool, Row, Sqlite};
 use log::error;
 
+#[derive(Debug, Clone)]
+pub struct CharacterCacheItem {
+    pub name: String,
+    pub user_id: u64,
+    pub guild_id: u64,
+    autocomplete_name: String,
+}
+
+impl CharacterCacheItem {
+    pub fn new(name: String, user_id: u64, guild_id: u64) -> Self {
+        CharacterCacheItem {
+            user_id,
+            guild_id,
+            name: name.clone(),
+            autocomplete_name: name,
+        }
+    }
+
+    pub fn get_autocomplete_name(&self) -> &String {
+        &self.autocomplete_name
+    }
+}
+
 pub struct Cache {
-    character_names: Mutex<Vec<String>>,
+    character_cache: Mutex<Vec<CharacterCacheItem>>,
 }
 
 impl Cache {
     pub fn new() -> Cache {
         Cache {
-            character_names: Mutex::new(Vec::new()),
+            character_cache: Mutex::new(Vec::new()),
         }
     }
 
-    pub async fn get_character_names(&self) -> Vec<String> {
-        self.character_names.lock().await.clone()
+    pub async fn get_characters(&self) -> Vec<CharacterCacheItem> {
+        self.character_cache.lock().await.clone()
     }
 
     pub async fn update_character_names(&self, db: &Pool<Sqlite>) {
-        let entries = sqlx::query("SELECT name FROM character")
+        let entries = sqlx::query("SELECT name, user_id, guild_id FROM character")
             .fetch_all(db).await;
 
         if let Ok(entries) = entries {
-            let mut names = self.character_names.lock().await;
-            names.clear();
-            names.extend(entries.iter().map(|x| x.get::<String, usize>(0)));
+            let mut cache = self.character_cache.lock().await;
+            cache.clear();
+            for x in entries {
+                cache.push(CharacterCacheItem::new(
+                    x.get::<String, usize>(0),
+                    x.get::<i64, usize>(1) as u64,
+                    x.get::<i64, usize>(2) as u64,
+                ))
+            }
         } else {
             error!("Was unable to update character names!");
         }
--- src/commands/autocompletion.rs
+++ src/commands/autocompletion.rs
@@ -1,26 +1,25 @@
 use std::cmp::Ordering;
 use crate::commands::Context;
 
-fn autocomplete(partial: &str, commands: &Vec<String>, minimum_query_length : usize) -> Vec<String> {
+fn filter_and_sort<'a>(partial: &str, commands: impl Iterator<Item = &'a String>, minimum_query_length : usize) -> Vec<String> {
     if partial.len() < minimum_query_length {
         return Vec::default();
     }
 
-    let lower_case = &partial.to_lowercase();
-
-    let mut result: Vec<String> = commands.iter()
-        .filter(move |x| x.to_lowercase().contains(lower_case))
+    let lowercase_user_input = &partial.to_lowercase();
+    let mut result: Vec<String> = commands
+        .filter(move |x| x.to_lowercase().contains(lowercase_user_input))
         .cloned()
         .collect();
 
     result.sort_by(|a, b| {
-        if a.to_lowercase().starts_with(lower_case) {
-            if b.to_lowercase().starts_with(lower_case) {
+        if a.to_lowercase().starts_with(lowercase_user_input) {
+            if b.to_lowercase().starts_with(lowercase_user_input) {
                 return a.cmp(b);
             }
             return Ordering::Less;
         }
-        if b.to_lowercase().starts_with(lower_case) {
+        if b.to_lowercase().starts_with(lowercase_user_input) {
             return Ordering::Greater;
         }
 
@@ -31,59 +30,73 @@ fn autocomplete(partial: &str, commands: &Vec<String>, minimum_query_length : us
 }
 
 pub async fn autocomplete_move<'a>(
-    _ctx: Context<'a>,
+    ctx: Context<'a>,
     partial: &'a str,
 ) -> Vec<String> {
-    autocomplete(partial, &_ctx.data().game.move_names, 2)
+    filter_and_sort(partial, ctx.data().game.move_names.iter(), 2)
 }
 
 pub async fn autocomplete_ability<'a>(
-    _ctx: Context<'a>,
+    ctx: Context<'a>,
     partial: &'a str,
 ) -> Vec<String> {
-    autocomplete(partial, &_ctx.data().game.ability_names, 2)
+    filter_and_sort(partial, ctx.data().game.ability_names.iter(), 2)
 }
 
 pub async fn autocomplete_pokemon<'a>(
-    _ctx: Context<'a>,
+    ctx: Context<'a>,
     partial: &'a str,
 ) -> Vec<String> {
-    autocomplete(partial, &_ctx.data().game.pokemon_names, 2)
+    filter_and_sort(partial, ctx.data().game.pokemon_names.iter(), 2)
 }
 
 pub async fn autocomplete_item<'a>(
-    _ctx: Context<'a>,
+    ctx: Context<'a>,
     partial: &'a str,
-) -> Vec<String> { autocomplete(partial, &_ctx.data().game.item_names, 2) }
+) -> Vec<String> { filter_and_sort(partial, ctx.data().game.item_names.iter(), 2) }
 
 pub async fn autocomplete_weather<'a>(
-    _ctx: Context<'a>,
+    ctx: Context<'a>,
     partial: &'a str,
-) -> Vec<String> { autocomplete(partial, &_ctx.data().game.weather_names, 0) }
+) -> Vec<String> { filter_and_sort(partial, ctx.data().game.weather_names.iter(), 0) }
 
 pub async fn autocomplete_status_effect<'a>(
-    _ctx: Context<'a>,
+    ctx: Context<'a>,
     partial: &'a str,
-) -> Vec<String> { autocomplete(partial, &_ctx.data().game.status_effects_names, 0) }
+) -> Vec<String> { filter_and_sort(partial, ctx.data().game.status_effects_names.iter(), 0) }
 
 pub async fn autocomplete_rule<'a>(
-    _ctx: Context<'a>,
+    ctx: Context<'a>,
     partial: &'a str,
-) -> Vec<String> { autocomplete(partial, &_ctx.data().game.rule_names, 0) }
+) -> Vec<String> { filter_and_sort(partial, ctx.data().game.rule_names.iter(), 0) }
 
 pub async fn autocomplete_nature<'a>(
-    _ctx: Context<'a>,
+    ctx: Context<'a>,
     partial: &'a str,
-) -> Vec<String> { autocomplete(partial, &_ctx.data().game.nature_names, 0) }
+) -> Vec<String> { filter_and_sort(partial, ctx.data().game.nature_names.iter(), 0) }
 
 pub async fn autocomplete_potion<'a>(
-    _ctx: Context<'a>,
+    ctx: Context<'a>,
     partial: &'a str,
-) -> Vec<String> { autocomplete(partial, &_ctx.data().game.potion_names, 0) }
+) -> Vec<String> { filter_and_sort(partial, ctx.data().game.potion_names.iter(), 0) }
 
 pub async fn autocomplete_character_name<'a>(
-    _ctx: Context<'a>,
+    ctx: Context<'a>,
     partial: &'a str,
 ) -> Vec<String> {
-    autocomplete(partial, &_ctx.data().cache.get_character_names().await, 0)
+    filter_and_sort(partial, ctx.data().cache.get_characters().await
+        .iter()
+        .filter(|x| x.guild_id == ctx.guild_id().expect("Command should be guild_only!").0)
+        .map(|x| x.get_autocomplete_name()), 0)
+}
+
+pub async fn autocomplete_owned_character_name<'a>(
+    ctx: Context<'a>,
+    partial: &'a str,
+) -> Vec<String> {
+    filter_and_sort(partial, ctx.data().cache.get_characters().await
+        .iter()
+        .filter(|x| x.user_id == ctx.author().id.0)
+        .filter(|x| x.guild_id == ctx.guild_id().expect("Command should be guild_only!").0)
+        .map(|x| x.get_autocomplete_name()), 0)
 }
