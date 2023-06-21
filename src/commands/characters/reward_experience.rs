8358c3e92077552d32a413c6ac0dbb91b1f1f1fd
jacudibu <jacudibu@gmail.com>
2023-06-21 13:25:36 +0200

    smol refactor

--- src/commands/characters/reward_experience.rs
+++ src/commands/characters/reward_experience.rs
@@ -1,5 +1,5 @@
-use crate::commands::{Context, Error, send_error};
-use crate::commands::characters::{ActionType, change_character_stat, validate_user_input};
+use crate::commands::{Context, Error};
+use crate::commands::characters::{ActionType, change_character_stat};
 use crate::commands::autocompletion::autocomplete_character_name;
 
 /// Reward players with cash.
@@ -12,7 +12,7 @@ pub async fn reward_experience(
     name: String,
 ) -> Result<(), Error> {
     // TODO: Button to undo the transaction which lasts for a minute or so.
-    if let Ok(result) = change_character_stat(&ctx, "experience", &name, amount as i64, ActionType::Reward).await {
+    if let Ok(_) = change_character_stat(&ctx, "experience", &name, amount as i64, ActionType::Reward).await {
         ctx.say(format!("{} received {} experience points!", name, amount)).await?;
     }
 
--- src/commands/characters/reward_money.rs
+++ src/commands/characters/reward_money.rs
@@ -1,6 +1,6 @@
 use crate::emoji;
-use crate::commands::{Context, Error, send_error};
-use crate::commands::characters::{ActionType, change_character_stat, validate_user_input};
+use crate::commands::{Context, Error};
+use crate::commands::characters::{ActionType, change_character_stat};
 use crate::commands::autocompletion::autocomplete_character_name;
 
 /// Reward players with cash.
@@ -14,7 +14,7 @@ pub async fn reward_money(
 ) -> Result<(), Error> {
     // TODO: Option to also add the untaxed amount to guild stash.
     // TODO: Button to undo the transaction which lasts for a minute or so.
-    if let Ok(result) = change_character_stat(&ctx, "money", &name, amount as i64, ActionType::Reward).await {
+    if let Ok(_) = change_character_stat(&ctx, "money", &name, amount as i64, ActionType::Reward).await {
         ctx.say(format!("{} received {} {}!", name, amount, emoji::POKE_COIN)).await?;
     }
 
