dee112ec11d0bc3ec75825088fac0768c0a1db41
jacudibu <jacudibu@gmail.com>
2023-06-22 12:14:40 +0200

    making clippy happy

--- src/csv_utils.rs
+++ src/csv_utils.rs
@@ -6,14 +6,12 @@ pub fn load_csv<P: AsRef<Path>, T: DeserializeOwned>(path: P) -> Vec<T> {
 
     let reader = csv::ReaderBuilder::new().has_headers(true).from_path(path);
 
-    for result in reader.expect("").records() {
-        if let Ok(record) = result {
-            let value: T = record.deserialize(None).expect("Unable to parse csv row");
-            results.push(value);
-        };
+    for record in reader.expect("").records().flatten() {
+        let value: T = record.deserialize(None).expect("Unable to parse csv row");
+        results.push(value);
     }
 
-    return results;
+    results
 }
 
 pub fn load_csv_with_custom_headers<P: AsRef<Path>, T: DeserializeOwned>(
@@ -24,14 +22,12 @@ pub fn load_csv_with_custom_headers<P: AsRef<Path>, T: DeserializeOwned>(
     let reader = csv::ReaderBuilder::new().has_headers(false).from_path(path);
 
     let header_records = csv::StringRecord::from(headers);
-    for row in reader.expect("").records() {
-        if let Ok(record) = row {
-            let item: T = record
-                .deserialize(Some(&header_records))
-                .expect("Csv should be parsable!");
-            result.push(item);
-        };
+    for record in reader.expect("").records().flatten() {
+        let item: T = record
+            .deserialize(Some(&header_records))
+            .expect("Csv should be parsable!");
+        result.push(item);
     }
 
-    return result;
+    result
 }
