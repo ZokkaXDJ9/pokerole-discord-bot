dc672b513d878110cd10d84a94009d7dbe2e6110
jacudibu <jacudibu@gmail.com>
2023-07-14 09:52:02 +0200

    Added a lazy migration command

--- src/commands/quests/mod.rs
+++ src/commands/quests/mod.rs
@@ -5,6 +5,7 @@ use poise::Command;
 mod add_quest_participant;
 mod complete_quest;
 mod create_quest;
+mod migrate_db_to_new_quest_system;
 mod remove_quest_participant;
 
 pub fn get_all_commands() -> Vec<Command<Data, Error>> {
@@ -13,5 +14,6 @@ pub fn get_all_commands() -> Vec<Command<Data, Error>> {
         complete_quest::complete_quest(),
         create_quest::create_quest(),
         remove_quest_participant::remove_quest_participant(),
+        migrate_db_to_new_quest_system::migrate_db_to_new_quest_system(),
     ]
 }
