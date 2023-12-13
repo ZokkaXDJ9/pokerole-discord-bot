db56acf3fb649d3352b13c21f3b82a0e671f61fe
jacudibu <jacudibu@gmail.com>
2023-12-13 09:17:41 +0100

    updated /learns to the new serenity builder style

--- src/commands/learns.rs
+++ src/commands/learns.rs
@@ -3,6 +3,7 @@ use crate::commands::{Context, Error};
 use crate::game_data::pokemon::Pokemon;
 use crate::helpers;
 use poise::CreateReply;
+use serenity::all::CreateActionRow;
 
 /// Display Pokemon moves
 #[poise::command(slash_command, prefix_command)]
@@ -14,28 +15,25 @@ pub async fn learns(
     name: String,
 ) -> Result<(), Error> {
     if let Some(pokemon) = ctx.data().game.pokemon.get(&name.to_lowercase()) {
-        ctx.send(|b| create_reply(b, pokemon)).await?;
+        ctx.send(create_reply(pokemon)).await?;
     } else {
-        ctx.send(|b| {
-            b.content(std::format!("Unable to find a pokemon named **{}**, sorry! If that wasn't a typo, maybe it isn't implemented yet?", name));
-            b.ephemeral(true)
-        }).await?;
+        ctx.send(CreateReply::default()
+            .content(std::format!("Unable to find a pokemon named **{}**, sorry! If that wasn't a typo, maybe it isn't implemented yet?", name))
+            .ephemeral(true)
+        ).await?;
     }
 
     Ok(())
 }
 
-pub fn create_reply<'a, 'b>(
-    b: &'a mut CreateReply<'b>,
-    pokemon: &Pokemon,
-) -> &'a mut CreateReply<'b> {
-    b.content(pokemon.build_move_string()).components(|b| {
-        b.create_action_row(|b| {
-            b.add_button(helpers::create_button(
+pub fn create_reply(pokemon: &Pokemon) -> CreateReply {
+    CreateReply::default()
+        .content(pokemon.build_move_string())
+        .components(vec![CreateActionRow::Buttons(vec![
+            helpers::create_button(
                 "Show All Learnable Moves",
                 format!("learns-all_{}", pokemon.name.to_lowercase()).as_str(),
                 false,
-            ))
-        })
-    })
+            ),
+        ])])
 }
