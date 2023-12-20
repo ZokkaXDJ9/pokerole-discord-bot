04e3ee2e5733cd4ec44966fa1a016e68754de657
jacudibu <jacudibu@gmail.com>
2023-12-20 16:40:17 +0100

    Updated /about text

--- src/commands/about.rs
+++ src/commands/about.rs
@@ -7,10 +7,12 @@ pub async fn about(ctx: Context<'_>) -> Result<(), Error> {
     ctx.say("\
 ```k4rpOS v0x3b```
 Made with love, cookies and <:ferris_happy:1120237205013663804>!
+**You may use this bot on other discord servers as you see fit. Character data is server specific, so there's no need to worry about breaking anything!**
+For help and support, check out the bot development & support discord server: <https://discord.gg/jVrv2YG2zU>
+
 ## Semi-Useful Links
-- Bot development & support discord server: <https://discord.gg/jVrv2YG2zU>
-- [Source code](<https://github.com/Jacudibu/pokerole-discord-bot>)
-- [Custom Bot Data](<https://github.com/Jacudibu/pokerole-custom-data>)
+- Source Code: [GitHub Repository](<https://github.com/Jacudibu/pokerole-discord-bot>)
+- Custom Bot Data: [GitHub Repository](<https://github.com/Jacudibu/pokerole-custom-data>)
 ### External Data Sources
 - **Original Pokerole System**: <https://www.pokeroleproject.com>
 - Pokerole specific data is taken from **Pokerole-Data** [[Link](<https://github.com/Pokerole-Software-Development/Pokerole-Data>)]
