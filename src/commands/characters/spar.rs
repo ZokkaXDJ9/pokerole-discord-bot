57bf888f9fa0e531355ee911f27d04fb409b0138
jacudibu <jacudibu@gmail.com>
2023-12-25 11:52:20 +0100

    Refactoring

--- src/commands/characters/spar.rs
+++ src/commands/characters/spar.rs
@@ -34,18 +34,18 @@ pub async fn spar(
 
     match handle_sparring(&ctx, &args).await {
         Ok(result) => {
-            if result.participants_who_gained_exp.len() > 0 {
+            if result.participants_who_gained_exp.is_empty() {
+                ctx.say(format!(
+                    "Tracked a sparring session for {}.",
+                    build_character_list(&result.participants),
+                ))
+                .await?;
+            } else {
                 ctx.say(format!(
                     "Tracked a sparring session for {}.\n{} received {} experience points!",
                     build_character_list(&result.participants),
                     build_character_list(&result.participants_who_gained_exp),
                     result.experience_value,
-                ))
-                .await?;
-            } else {
-                ctx.say(format!(
-                    "Tracked a sparring session for {}.",
-                    build_character_list(&result.participants),
                 ))
                 .await?;
             }
@@ -64,6 +64,11 @@ struct SparringResult {
     experience_value: i64,
 }
 
+struct SparSettings {
+    weekly_spar_limit: i64,
+    weekly_spar_reward: i64,
+}
+
 async fn handle_sparring<'a>(
     ctx: &Context<'a>,
     names: &Vec<String>,
@@ -75,7 +80,8 @@ async fn handle_sparring<'a>(
 
     let guild_id_i64 = guild_id as i64;
 
-    let guild_settings = sqlx::query!(
+    let spar_settings = sqlx::query_as!(
+        SparSettings,
         "SELECT weekly_spar_limit, weekly_spar_reward FROM guild WHERE id = ?",
         guild_id_i64
     )
@@ -88,53 +94,63 @@ async fn handle_sparring<'a>(
             let mut result = SparringResult {
                 participants: Vec::new(),
                 participants_who_gained_exp: Vec::new(),
-                experience_value: guild_settings.weekly_spar_reward,
+                experience_value: spar_settings.weekly_spar_reward,
             };
 
             for x in &characters {
-                result.participants.push(x.clone());
-                let current = sqlx::query!(
-                    "SELECT total_spar_count, weekly_spar_count FROM character WHERE id = ?",
-                    x.id
-                )
-                .fetch_one(&ctx.data().database)
-                .await
-                .expect("Character with cached ID should exist."); // TODO: Error handling
-
-                let new_total_spar_count = current.total_spar_count + 1;
-                let new_weekly_spar_count = current.weekly_spar_count + 1;
-
-                let _ = sqlx::query!(
-                    "UPDATE character SET total_spar_count = ?, weekly_spar_count = ? WHERE id = ?",
-                    new_total_spar_count,
-                    new_weekly_spar_count,
-                    x.id
-                )
-                .execute(&ctx.data().database)
-                .await;
-
-                let _ = log_action(
-                    &ActionType::Spar,
-                    ctx,
-                    &format!("Tracked a sparring session for {}!", x.name),
-                )
-                .await;
-
-                if new_total_spar_count <= guild_settings.weekly_spar_limit {
-                    result.participants_who_gained_exp.push(x.clone());
-                    let _ = change_character_stat_after_validation(
-                        ctx,
-                        "experience",
-                        x,
-                        guild_settings.weekly_spar_reward,
-                        &ActionType::Spar,
-                    )
-                    .await;
-                }
+                track_spar_for_character(ctx, &spar_settings, &mut result, x).await;
             }
+
             Ok(result)
         }
         Err(error) => Err(error),
+    }
+}
+
+async fn track_spar_for_character<'a>(
+    ctx: &Context<'a>,
+    guild_settings: &SparSettings,
+    result: &mut SparringResult,
+    x: &CharacterCacheItem,
+) {
+    result.participants.push(x.clone());
+    let current = sqlx::query!(
+        "SELECT total_spar_count, weekly_spar_count FROM character WHERE id = ?",
+        x.id
+    )
+    .fetch_one(&ctx.data().database)
+    .await
+    .expect("Character with cached ID should exist.");
+
+    let new_total_spar_count = current.total_spar_count + 1;
+    let new_weekly_spar_count = current.weekly_spar_count + 1;
+
+    let _ = sqlx::query!(
+        "UPDATE character SET total_spar_count = ?, weekly_spar_count = ? WHERE id = ?",
+        new_total_spar_count,
+        new_weekly_spar_count,
+        x.id
+    )
+    .execute(&ctx.data().database)
+    .await;
+
+    let _ = log_action(
+        &ActionType::Spar,
+        ctx,
+        &format!("Tracked a sparring session for {}!", x.name),
+    )
+    .await;
+
+    if new_total_spar_count <= guild_settings.weekly_spar_limit {
+        result.participants_who_gained_exp.push(x.clone());
+        let _ = change_character_stat_after_validation(
+            ctx,
+            "experience",
+            x,
+            guild_settings.weekly_spar_reward,
+            &ActionType::Spar,
+        )
+        .await;
     }
 }
 
