1fbb0396e4606cca709a28d2d04f13dc67970633
jacudibu <jacudibu@gmail.com>
2023-12-26 10:24:58 +0100

    Added species migration file

---
+++ migrations/20231226082442_character_pokemon_aware.sql
@@ -0,0 +1,60 @@
+ALTER TABLE character ADD COLUMN species_api_id INTEGER;
+ALTER TABLE character ADD COLUMN shiny BOOLEAN;
+
+UPDATE character SET species_api_id = 168, shiny = false WHERE id = 1;
+UPDATE character SET species_api_id = 445, shiny = false WHERE id = 2;
+UPDATE character SET species_api_id = 200, shiny = false WHERE id = 3;
+UPDATE character SET species_api_id = 248, shiny = false WHERE id = 4;
+UPDATE character SET species_api_id = 566, shiny = false WHERE id = 5;
+UPDATE character SET species_api_id = 921, shiny = false WHERE id = 6;
+UPDATE character SET species_api_id = 216, shiny = false WHERE id = 7;
+UPDATE character SET species_api_id = 627, shiny = false WHERE id = 8;
+UPDATE character SET species_api_id = 133, shiny = false WHERE id = 9;
+UPDATE character SET species_api_id = 133, shiny = false WHERE id = 10;
+UPDATE character SET species_api_id = 710, shiny = false WHERE id = 11;
+UPDATE character SET species_api_id = 215, shiny = false WHERE id = 12;
+UPDATE character SET species_api_id = 10179, shiny = false WHERE id = 13;
+UPDATE character SET species_api_id = 502, shiny = false WHERE id = 14;
+UPDATE character SET species_api_id = 783, shiny = false WHERE id = 15;
+UPDATE character SET species_api_id = 570, shiny = false WHERE id = 16;
+UPDATE character SET species_api_id = 447, shiny = false WHERE id = 17;
+UPDATE character SET species_api_id = 813, shiny = false WHERE id = 18;
+UPDATE character SET species_api_id = 747, shiny = false WHERE id = 19;
+UPDATE character SET species_api_id = 636, shiny = false WHERE id = 20;
+UPDATE character SET species_api_id = 403, shiny = false WHERE id = 21;
+UPDATE character SET species_api_id = 858, shiny = false WHERE id = 22;
+UPDATE character SET species_api_id = 155, shiny = false WHERE id = 23;
+UPDATE character SET species_api_id = 67, shiny = false WHERE id = 24;
+UPDATE character SET species_api_id = 258, shiny = false WHERE id = 25;
+UPDATE character SET species_api_id = 700, shiny = false WHERE id = 26;
+UPDATE character SET species_api_id = 161, shiny = false WHERE id = 27;
+UPDATE character SET species_api_id = 10103, shiny = false WHERE id = 28;
+UPDATE character SET species_api_id = 653, shiny = false WHERE id = 29;
+UPDATE character SET species_api_id = 190, shiny = false WHERE id = 30;
+UPDATE character SET species_api_id = 529, shiny = false WHERE id = 31;
+UPDATE character SET species_api_id = 327, shiny = false WHERE id = 32;
+UPDATE character SET species_api_id = 570, shiny = false WHERE id = 33;
+UPDATE character SET species_api_id = 200, shiny = false WHERE id = 34;
+UPDATE character SET species_api_id = 700, shiny = false WHERE id = 35;
+UPDATE character SET species_api_id = 10152, shiny = false WHERE id = 36;
+UPDATE character SET species_api_id = 304, shiny = false WHERE id = 37;
+UPDATE character SET species_api_id = 280, shiny = false WHERE id = 38;
+UPDATE character SET species_api_id = 935, shiny = false WHERE id = 39;
+UPDATE character SET species_api_id = 447, shiny = false WHERE id = 40;
+UPDATE character SET species_api_id = 255, shiny = false WHERE id = 41;
+UPDATE character SET species_api_id = 54, shiny = false WHERE id = 42;
+UPDATE character SET species_api_id = 307, shiny = false WHERE id = 43;
+UPDATE character SET species_api_id = 96, shiny = true WHERE id = 44;
+UPDATE character SET species_api_id = 123, shiny = false WHERE id = 45;
+UPDATE character SET species_api_id = 757, shiny = false WHERE id = 46;
+UPDATE character SET species_api_id = 352, shiny = false WHERE id = 47;
+UPDATE character SET species_api_id = 656, shiny = false WHERE id = 48;
+UPDATE character SET species_api_id = 10152, shiny = false WHERE id = 49;
+UPDATE character SET species_api_id = 757, shiny = false WHERE id = 50;
+UPDATE character SET species_api_id = 27, shiny = false WHERE id = 51;
+UPDATE character SET species_api_id = 1, shiny = false WHERE id = 52; -- debug character on test server
+UPDATE character SET species_api_id = 531, shiny = false WHERE id = 53;
+UPDATE character SET species_api_id = 4, shiny = false WHERE id = 54;
+
+
+
