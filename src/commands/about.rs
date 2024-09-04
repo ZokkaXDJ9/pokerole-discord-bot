8879a357bfb7e9b4da95e2473ef9ec21e4ee96d2
Bahamut <leander.volprich@web.de>
2024-09-04 23:11:05 +0200

    deleted last few instances of Karp

--- src/commands/about.rs
+++ src/commands/about.rs
@@ -5,17 +5,17 @@ use crate::commands::{Context, Error};
 pub async fn about(ctx: Context<'_>) -> Result<(), Error> {
     // TODO: Might be funny to add an actual version string here once we use proper tags and git actions and such :>
     ctx.say("\
-```k4rpOS v0x3b```
-Made with love, cookies and <:ferris_happy:1120237205013663804>!
+```Omniscient Xatu Guide```
+Made with love, cookies and <:ferrishappy:1272535933505503232>!
 **You may use this bot on other discord servers as you see fit. Character data is server specific, so there's no need to worry about breaking anything!**
-For help and support, check out the bot development & support discord server: <https://discord.gg/jVrv2YG2zU>
+For help and support, check out the bot development & support discord server of the original bot we used to build our own: <https://discord.gg/jVrv2YG2zU>
 
 ## Semi-Useful Links
-- Source Code: [GitHub Repository](<https://github.com/Jacudibu/pokerole-discord-bot>)
-- Custom Bot Data: [GitHub Repository](<https://github.com/Jacudibu/pokerole-custom-data>)
+- Source Code: [GitHub Repository](<https://github.com/ZokkaXDJ9/pokerole-discord-bot>)
+- Custom Bot Data: [GitHub Repository](<https://github.com/ZokkaXDJ9/pokerole-custom-data>)
 ### External Data Sources
 - **Original Pokerole System**: <https://www.pokeroleproject.com>
-- Pokerole specific data is taken from **Pokerole-Data** [[Link](<https://github.com/Pokerole-Software-Development/Pokerole-Data>)]
+- Pokerole specific data is taken from **Pokerole-Data** and modified by us [[Link](<https://github.com/Pokerole-Software-Development/Pokerole-Data>)]
 - Raw pokemon data taken from **pokeAPI** [[Link](<https://github.com/PokeAPI/pokeapi>) – [License](<https://github.com/PokeAPI/pokeapi/blob/master/LICENSE.md>)]
 Thanks to all of these projects for the effort they put in! <3
 "
--- src/commands/say.rs
+++ src/commands/say.rs
@@ -1,6 +1,6 @@
 use crate::commands::{Context, Error};
 
-/// Make the k4rpOS say something.
+/// Make Xatu say something.
 #[poise::command(slash_command, default_member_permissions = "ADMINISTRATOR")]
 pub async fn say(ctx: Context<'_>, text: String) -> Result<(), Error> {
     ctx.say(format!("```[{}]```", text)).await?;
